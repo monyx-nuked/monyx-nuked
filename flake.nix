fefa3ecf0f075fd9c9c5bf2fb78ab3c791cd583f
monyx-nuked <helix.nuked@proton.me>
2026-03-12 13:08:17 +0000

    add substitutors as a note

--- flake.nix
+++ flake.nix
@@ -3,6 +3,12 @@
     development shell for my github profile portofolio
   '';
   nixConfig = {
+    extra-substituters = [
+      # "https://nix-community.cachix.org"
+    ];
+    extra-trusted-public-keys = [
+      # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+    ];
     extra-experimental-features = ["nix-command" "flakes" "pipe-operators"];
     cores = 0;
   };
