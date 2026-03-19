d0568afdc6c8ffcf0849b0be819127f5586af19d
monyx <helix.nuked@proton.me>
2026-03-19 18:33:50 +0000

    format: nixpkgs-fmt

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
     extra-trusted-public-keys = [
       # "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
-    extra-experimental-features = ["nix-command" "flakes" "pipe-operators"];
+    extra-experimental-features = [ "nix-command" "flakes" "pipe-operators" ];
     cores = 0;
   };
   inputs = {
@@ -18,37 +18,39 @@
     ###########
     nixpkgs.url = "https://flakehub.com/f/nixos/nixpkgs/0.1";
   };
-  outputs = {nixpkgs, ...}: let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      inherit system;
-      config.allowUnfree = true;
+  outputs = { nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "profile-dev";
+        packages = with pkgs; [
+          helix
+          deadnix
+          nil
+          mdformat
+          git
+          ripgrep
+          jq
+          tree
+          gh
+          nh
+          nix-melt
+          manix
+          dix
+          nix-output-monitor
+          nix-tree
+          markdown-oxide
+        ];
+        shellHook = ''
+          echo "Welcome to the profile dev shell!"
+          echo "System: ${system}"
+        '';
+      };
     };
-  in {
-    devShells.${system}.default = pkgs.mkShell {
-      name = "profile-dev";
-      packages = with pkgs; [
-        helix
-        deadnix
-        nil
-        mdformat
-        git
-        ripgrep
-        jq
-        tree
-        gh
-        nh
-        nix-melt
-        manix
-        dix
-        nix-output-monitor
-        nix-tree
-        markdown-oxide
-      ];
-      shellHook = ''
-        echo "Welcome to the profile dev shell!"
-        echo "System: ${system}"
-      '';
-    };
-  };
 }
